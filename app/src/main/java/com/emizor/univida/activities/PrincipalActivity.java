25ee8c224a28c5a38991ace3ea2abd770a0256bc
UNIVIDA\lchavez <lchavez@univida.bo>
2025-02-03 18:01:55 -0400

    adicion de fragment

--- app/src/main/java/com/emizor/univida/activities/PrincipalActivity.java
+++ app/src/main/java/com/emizor/univida/activities/PrincipalActivity.java
@@ -562,7 +562,7 @@ public class PrincipalActivity extends RootActivity implements NavigationView.On
             if (isConnected) {
                 Runtime runtime = Runtime.getRuntime();
                 try {
-                    Process ipProcess = runtime.exec("/system/bin/ping -c 1 127.0.0.1");
+                    Process ipProcess = runtime.exec("/system/bin/ping -c 1 8.8.8.8");
                     int exitValue = ipProcess.waitFor();
                     return (exitValue == 0);
                 } catch (IOException e) {
